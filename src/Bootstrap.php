00e368973666b2011447685c6e8ff052e01752df
Mark Bellingham <mark.bellingham@protonmail.ch>
2021-01-05 13:50:50 +0000

    Ch7 Inversion of Control

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -70,7 +70,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className;
+        $class = new $className($response);
         $class->$method($vars);
 //        call_user_func($handler, $vars);
         break;
